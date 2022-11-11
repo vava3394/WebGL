7b536c5faeefcfebac47b9d8598915e2aaff13f2
valentinportal <portalvalentin@gmail.com>
2022-11-11 16:53:34 +0100

    petite modification

--- shaders/mirroir.fs
+++ shaders/mirroir.fs
@@ -23,7 +23,7 @@ void main() {
   vec3 directionR = mat3(uInverseRotMatrix)*reflect(-Vo,worldNormal);
   vec3 directionT = mat3(uInverseRotMatrix)*refract(-Vo,worldNormal,1.0/ni);
 
-  if(uIsTransparence){
+  if (uIsMirroir && uIsTransparence){
     float c = dot(Vo,worldNormal);
     float g = sqrt(ni*ni+c*c-1.0);
     float gmc = g-c;
@@ -32,15 +32,12 @@ void main() {
     float cgmc = c*(gmc)+1.0;
     R = 0.5*(gmc*gmc)/(gpc*gpc)*(1.0+(cgpc*cgpc)/(cgmc*cgmc));
     T = 1.0-R;
-  }
 
-  if(uIsMirroir && uIsTransparence ){
     vec4 colR = vec4(textureCube(u_skybox, directionR.xzy).xyz*R,1.0);
     vec4 colT = vec4(textureCube(u_skybox, directionT.xzy).xyz*T,1.0);
 
     gl_FragColor = vec4((colT+colR).xyz*uColorObj,1);
-  }
-  else if(uIsMirroir){
+  }else if(uIsMirroir){
     gl_FragColor = vec4(textureCube(u_skybox, directionR.xzy).xyz*uColorObj,1.0);
   }else if (uIsTransparence){
     gl_FragColor = vec4(textureCube(u_skybox, directionT.xzy).xyz*uColorObj,1.0);
