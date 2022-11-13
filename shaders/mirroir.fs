5ed74156da45e220d90375a0b25b65c0182c4b65
valentinportal <portalvalentin@gmail.com>
2022-11-13 15:25:10 +0100

    test

--- shaders/mirroir.fs
+++ shaders/mirroir.fs
@@ -1,5 +1,7 @@
 precision mediump float;
 
+#define pi = 3.14;
+
 // postions et normales
 varying vec4 pos3D;
 varying vec3 N;
@@ -15,23 +17,38 @@ uniform vec3 uColorObj;
 
 void main() {
   float ni= uRatio;
-  float R;
-  float T;
+  float R,T,D,theta,phi;
+  float alpha,rand1,rand2;
 
   vec3 worldNormal = normalize(N);
   vec3 Vo = normalize(-pos3D.xyz);
   vec3 directionR = mat3(uInverseRotMatrix)*reflect(-Vo,worldNormal);
   vec3 directionT = mat3(uInverseRotMatrix)*refract(-Vo,worldNormal,1.0/ni);
 
+  float c = dot(Vo,worldNormal);
+  float g = sqrt(ni*ni+c*c-1.0);
+  float gmc = g-c;
+  float gpc = g+c;
+  float cgpc = c*(gpc)-1.0;
+  float cgmc = c*(gmc)+1.0;
+  R = 0.5*(gmc*gmc)/(gpc*gpc)*(1.0+(cgpc*cgpc)/(cgmc*cgmc));
+  T = 1.0-R;
+
+  for(int i=0,i<20,i++){
+    phi = rand1*2.0*pi
+  }
+
+  D = 
+
+
+  vec3 fr = (1.0-F)*(uColorObj/pi)+
+
+
+
+
+
   if (uIsMirroir && uIsTransparence){
-    float c = dot(Vo,worldNormal);
-    float g = sqrt(ni*ni+c*c-1.0);
-    float gmc = g-c;
-    float gpc = g+c;
-    float cgpc = c*(gpc)-1.0;
-    float cgmc = c*(gmc)+1.0;
-    R = 0.5*(gmc*gmc)/(gpc*gpc)*(1.0+(cgpc*cgpc)/(cgmc*cgmc));
-    T = 1.0-R;
+    
 
     vec4 colR = vec4(textureCube(u_skybox, directionR.xzy).xyz*R,1.0);
     vec4 colT = vec4(textureCube(u_skybox, directionT.xzy).xyz*T,1.0);
